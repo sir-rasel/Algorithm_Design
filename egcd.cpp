2667ab447a2c6ce8c1eb7f9d97017a1f8cebdaf4
unknown <raselcse97@gmail.com>
2019-07-18 17:42:37 +0600

    Iteratice Extended GCD code added

--- egcd.cpp
+++ egcd.cpp
@@ -15,6 +15,21 @@ int egcd(int a,int b,int &x,int &y,int i){
     return d;
 }
 
+// Iterative extended GCD
+int ext_gcd(int A,int B,int *X,int *Y){
+    ll x2,y2,x1,y1,x,y,r2,r1,q,r;
+    x2=1;y2=0;
+    x1=0;y1=1;
+    for(r2=A,r1=B;r1!=0;r2=r1,r1=r,x2=x1,y2=y1,x1=x,y1=y){
+        q=r2/r1;
+        r=r2%r1;
+        x=x2-(q*x1);
+        y=y2-(q*y1);
+    }
+    *X=x2;*Y=y2;
+    return r2;
+}
+
 int main(){
     int x,y;
     int g = egcd(10,6,x,y,1);
